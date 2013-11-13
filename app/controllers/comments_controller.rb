451b702729fa02ff1be36a276d1c7f54a51336a6
chasm <charles.munat@gmail.com>
2013-11-13 18:46:47 +0800

    Initial models and JSON controllers, plus simple site page w/ajax.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,38 @@
+class CommentsController < ApplicationController
+  def index
+    article = Article.includes(:comments).find(params[:article_id])
+    comments = article.comments
+    
+    render json: comments.to_json(
+      include: [
+        article: {
+          only: [ :title ]
+        }
+      ],
+      except: [ :updated_at ]
+    )
+  end
+
+  def show
+    article = Article.includes(:comments).find(params[:article_id])
+    comment = article.comments.find(params[:id])
+    
+    render json: comment.to_json(
+      include: [
+        article: {
+          only: [ :title ]
+        }
+      ],
+      except: [ :updated_at ]
+    )
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
